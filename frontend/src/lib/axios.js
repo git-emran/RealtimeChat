2be7466b5f846d8143702d88464584880f932df2
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-24 08:37:19 +0600

    adding api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "api",
+      : "/api",
 
   withCredentials: true,
 });
